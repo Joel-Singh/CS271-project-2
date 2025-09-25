f3ffa9ec0fe461aebabdd189f2b2a4a18d12ea3f
cnguyen3749 <colin.nguyen.3749@gmail.com>
2025-09-25 19:04:05 -0400

    Create header and .cpp for MinQueue file

---
+++ MinQueue.h
@@ -0,0 +1,34 @@
+//=========================================
+// Joel Singh & Colin Nguyen
+// September 2025
+// MinQueue.h
+// This file contains all the method 
+// declarations for the MinQueue class
+//=========================================
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
+#ifndef MINQUEUE_H
+#define MINQUEUE_H
+
+template <class T> 
+class MinQueue
+{
+	public:
+            MinQueue                (  );
+            ~MinQueue               (  );
+            MinQueue                ( T* A, int n );
+	void    insert					( const T& item );
+    T       min                     (  );
+    T       extract_min             (  );
+    void    decrease                ( int i, T k );
+
+};
+
+
+#include "MinQueue.cpp"
+
+#endif
