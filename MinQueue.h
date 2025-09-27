ba57ecf1287b27fe0fca3666e6059452401c083e
Joel Singh <joelsingh788@gmail.com>
2025-09-27 13:08:27 -0400

    Format

--- MinQueue.h
+++ MinQueue.h
@@ -2,32 +2,28 @@
 // Joel Singh & Colin Nguyen
 // September 2025
 // MinQueue.h
-// This file contains all the method 
+// This file contains all the method
 // declarations for the MinQueue class
 //=========================================
 
 #include <iostream>
-#include <string>
 #include <sstream>
 #include <stdlib.h>
+#include <string>
 
 #ifndef MINQUEUE_H
 #define MINQUEUE_H
 
-template <class T> 
-class MinQueue
-{
-	public:
-            MinQueue                (  );
-            ~MinQueue               (  );
-            MinQueue                ( T* A, int n );
-	void    insert					( const T& item );
-    T       min                     (  );
-    T       extract_min             (  );
-    void    decrease                ( int i, T k );
-
+template <class T> class MinQueue {
+public:
+  MinQueue();
+  ~MinQueue();
+  MinQueue(T *A, int n);
+  void insert(const T &item);
+  T min();
+  T extract_min();
+  void decrease(int i, T k);
 };
-
 
 #include "MinQueue.cpp"
 
--- test_minqueue_example.cpp
+++ test_minqueue_example.cpp
@@ -4,9 +4,9 @@
 //  Project 2
 //
 // ************************************
-#include <iostream>
 #include "MinQueue.h"
 #include <chrono>
+#include <iostream>
 #include <stdexcept>
 
 using namespace std;
@@ -28,7 +28,8 @@ using namespace std;
 //
 //         if (mq_str != "")
 //         {
-//             cout << "Incorrect result from empty constructor. Expected an empty string but got : " << mq_str << endl;
+//             cout << "Incorrect result from empty constructor. Expected an
+//             empty string but got : " << mq_str << endl;
 //         }
 //
 //         MinQueue<int> mq(int_data, 10);
@@ -36,7 +37,8 @@ using namespace std;
 //
 //         if (mq_str != "1 2 4 3 6 5 8 10 7 9")
 //         {
-//             cout << "Incorrect result from empty constructor. Expected 1 2 4 3 6 5 8 10 7 9 but got : " << mq_str << endl;
+//             cout << "Incorrect result from empty constructor. Expected 1 2 4
+//             3 6 5 8 10 7 9 but got : " << mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
@@ -65,7 +67,8 @@ using namespace std;
 //
 //         if (mq_str != "0")
 //         {
-//             cout << "Incorrect insert result. Epected 0 but got : " << mq_str << endl;
+//             cout << "Incorrect insert result. Epected 0 but got : " << mq_str
+//             << endl;
 //         }
 //
 //         MinQueue<int> mq(int_data, 10);
@@ -75,12 +78,14 @@ using namespace std;
 //
 //         if (mq_str != "0 2 1 3 6 4 8 10 7 9 25 5")
 //         {
-//             cout << "Incorrect insert result. Expected 0 2 1 3 6 4 8 10 7 9 25 5 but got : " << mq_str << endl;
+//             cout << "Incorrect insert result. Expected 0 2 1 3 6 4 8 10 7 9
+//             25 5 but got : " << mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
 //     {
-//         cerr << "Error inserting into the priority queue : " << e.what() << endl;
+//         cerr << "Error inserting into the priority queue : " << e.what() <<
+//         endl;
 //     }
 //
 //     delete[] int_data;
@@ -104,7 +109,8 @@ using namespace std;
 //     catch (exception &e)
 //     {
 //         // expect an underflow error
-//         cout << "Correctly caught error trying to get min from empty queue: " << e.what() << endl;
+//         cout << "Correctly caught error trying to get min from empty queue: "
+//         << e.what() << endl;
 //     }
 //
 //     try
@@ -113,12 +119,14 @@ using namespace std;
 //         int min = mq.min();
 //         if (min != 1)
 //         {
-//             cout << "Incorrect min result. Expect 0 but got : " << min << endl;
+//             cout << "Incorrect min result. Expect 0 but got : " << min <<
+//             endl;
 //         }
 //     }
 //     catch (exception &e)
 //     {
-//         cerr << "Error in determining min of the priority queue : " << e.what() << endl;
+//         cerr << "Error in determining min of the priority queue : " <<
+//         e.what() << endl;
 //     }
 //
 //     delete[] int_data;
@@ -137,12 +145,14 @@ using namespace std;
 //         MinQueue<int> empty;
 //         int min = empty.extract_min();
 //         string mq_str = empty.to_string();
-//         cout << "Incorrectly extracted min from empty queue: " << min << endl;
+//         cout << "Incorrectly extracted min from empty queue: " << min <<
+//         endl;
 //     }
 //     catch (exception &e)
 //     {
 //         // expect an underflow error
-//         cout << "Correctly caught error trying to extract min from empty queue: " << e.what() << endl;
+//         cout << "Correctly caught error trying to extract min from empty
+//         queue: " << e.what() << endl;
 //     }
 //     try
 //     {
@@ -152,12 +162,15 @@ using namespace std;
 //
 //         if (min != 1 || mq_str != "2 3 4 7 6 5 8 10 9")
 //         {
-//             cout << "Incorrect extract min result. Expected 0 and the queue 2 3 4 7 6 5 8 10 9 but got : " << min << " and a queue of : " << mq_str << endl;
+//             cout << "Incorrect extract min result. Expected 0 and the queue 2
+//             3 4 7 6 5 8 10 9 but got : " << min << " and a queue of : " <<
+//             mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
 //     {
-//         cerr << "Error in determining min of the priority queue : " << e.what() << endl;
+//         cerr << "Error in determining min of the priority queue : " <<
+//         e.what() << endl;
 //     }
 //
 //     delete[] int_data;
@@ -181,7 +194,8 @@ using namespace std;
 //
 //         if (mq_str != "-6 -1 4 3 2 5 8 10 7 6")
 //         {
-//             cout << "Incorrect decrease key result. Expected empty queue but got : " << mq_str << endl;
+//             cout << "Incorrect decrease key result. Expected empty queue but
+//             got : " << mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
@@ -212,7 +226,8 @@ using namespace std;
 //
 //         if (mq_str != "")
 //         {
-//             cout << "Incorrect heapify result. Expected empty string but got : " << mq_str << endl;
+//             cout << "Incorrect heapify result. Expected empty string but got
+//             : " << mq_str << endl;
 //         }
 //
 //         MinQueue<int> mq(int_data, 10);
@@ -224,7 +239,9 @@ using namespace std;
 //
 //         if (mq_str != "1 3 4 7 6 5 8 10 11 9")
 //         {
-//             cout << "Incorrect heapify result in heapifying index 1 in the minqueue " << o_mq_str << " after setting to 11. Expected 1 3 4 7 6 5 8 10 11 9 but got : " << mq_str << endl;
+//             cout << "Incorrect heapify result in heapifying index 1 in the
+//             minqueue " << o_mq_str << " after setting to 11. Expected 1 3 4 7
+//             6 5 8 10 11 9 but got : " << mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
@@ -261,7 +278,8 @@ using namespace std;
 //
 //         if (mq_str != "1 2 4 3 6 5 8 10 7 9")
 //         {
-//             cout << "Incorrect build_heap result from " << o_mq_str << ". Expected 1 2 4 3 6 5 8 10 7 9 but got : " << mq_str << endl;
+//             cout << "Incorrect build_heap result from " << o_mq_str << ".
+//             Expected 1 2 4 3 6 5 8 10 7 9 but got : " << mq_str << endl;
 //         }
 //     }
 //     catch (exception &e)
@@ -301,7 +319,8 @@ using namespace std;
 //
 //         if (sorted_str != "1 2 3 4 5 6 7 8 9 10")
 //         {
-//             cout << "Incorrect heapsort result. Expected 1 2 3 4 5 6 7 8 9 10 but got : " << sorted_str << endl;
+//             cout << "Incorrect heapsort result. Expected 1 2 3 4 5 6 7 8 9 10
+//             but got : " << sorted_str << endl;
 //         }
 //     }
 //     catch (exception &e)
@@ -314,67 +333,78 @@ using namespace std;
 // }
 
 // use an enum to represent class year
-enum Year {SR, JR, SO, FR};
+enum Year { SR, JR, SO, FR };
 
 // define a struct representing a student record
-struct StudentRecord
-{
-    string name;
-    Year year;
-    // set registration time to the current time when the student record is created
-    std::chrono::system_clock::time_point registration_time = std::chrono::system_clock::now();
+struct StudentRecord {
+  string name;
+  Year year;
+  // set registration time to the current time when the student record is
+  // created
+  std::chrono::system_clock::time_point registration_time =
+      std::chrono::system_clock::now();
 
-    // overload < operator to compare students based on year and registration time
-    bool operator<(const StudentRecord &other) const
-    {
-        return (year < other.year) || (year == other.year && registration_time < other.registration_time);
-    }
+  // overload < operator to compare students based on year and registration time
+  bool operator<(const StudentRecord &other) const {
+    return (year < other.year) ||
+           (year == other.year && registration_time < other.registration_time);
+  }
 
-    // overload > operator to compare students based on year and registration time
-    bool operator>(const StudentRecord &other) const
-    {
-        return (year > other.year) || (year == other.year && registration_time > other.registration_time);
-    }
+  // overload > operator to compare students based on year and registration time
+  bool operator>(const StudentRecord &other) const {
+    return (year > other.year) ||
+           (year == other.year && registration_time > other.registration_time);
+  }
 
-    // overload == operator to compare students based on year and registration time
-    bool operator==(const StudentRecord &other) const
-    {
-        return (year == other.year && registration_time == other.registration_time);
-    }
+  // overload == operator to compare students based on year and registration
+  // time
+  bool operator==(const StudentRecord &other) const {
+    return (year == other.year && registration_time == other.registration_time);
+  }
 
-    // overload != operator to compare students based on year and registration time
-    bool operator!=(const StudentRecord &other) const 
-    {
-        return (year != other.year || registration_time != other.registration_time);
-    }
-    
-    // overload <= operator to compare students based on year and registration time
-    bool operator<=(const StudentRecord &other) const
-    {
-        return (year < other.year) || (year == other.year && registration_time <= other.registration_time);
-    }
+  // overload != operator to compare students based on year and registration
+  // time
+  bool operator!=(const StudentRecord &other) const {
+    return (year != other.year || registration_time != other.registration_time);
+  }
 
-    // overload >= operator to compare students based on year and registration time
-    bool operator>=(const StudentRecord &other) const
-    {
-        return (year > other.year) || (year == other.year && registration_time >= other.registration_time);
-    }
+  // overload <= operator to compare students based on year and registration
+  // time
+  bool operator<=(const StudentRecord &other) const {
+    return (year < other.year) ||
+           (year == other.year && registration_time <= other.registration_time);
+  }
 
-    // overload << operator to print student record
-    friend ostream &operator<<(ostream &os, const StudentRecord &student)
-    {
-        string year_str;
-        switch (student.year)   
-        {
-            case SR: year_str = "Senior"; break;
-            case JR: year_str = "Junior"; break;
-            case SO: year_str = "Sophomore"; break;
-            case FR: year_str = "Freshman"; break;
-            default: year_str = "Unknown"; break;
-        }
-        os << student.name << " (" << year_str << ")";
-        return os;
+  // overload >= operator to compare students based on year and registration
+  // time
+  bool operator>=(const StudentRecord &other) const {
+    return (year > other.year) ||
+           (year == other.year && registration_time >= other.registration_time);
+  }
+
+  // overload << operator to print student record
+  friend ostream &operator<<(ostream &os, const StudentRecord &student) {
+    string year_str;
+    switch (student.year) {
+    case SR:
+      year_str = "Senior";
+      break;
+    case JR:
+      year_str = "Junior";
+      break;
+    case SO:
+      year_str = "Sophomore";
+      break;
+    case FR:
+      year_str = "Freshman";
+      break;
+    default:
+      year_str = "Unknown";
+      break;
     }
+    os << student.name << " (" << year_str << ")";
+    return os;
+  }
 };
 
 // void test_application_waitlist()
@@ -396,8 +426,8 @@ struct StudentRecord
 //     waitlist.insert({"Olivia", SR});
 //     waitlist.insert({"Peggy", JR});
 //
-//     // process the waitlist and print the order in which students are registered
-//     cout << "Registration order:" << endl;
+//     // process the waitlist and print the order in which students are
+//     registered cout << "Registration order:" << endl;
 //
 //     // assume there are only 10 spots available
 //     for (int i = 0; i < 10; i++)
@@ -419,52 +449,51 @@ struct StudentRecord
 //     auto begin = std::chrono::high_resolution_clock::now();
 //     mq.insert(val);
 //     auto end = std::chrono::high_resolution_clock::now();
-//     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-//     cout << "insert time test took " << elapsed.count() << " nanoseconds" << endl;
-//     total += elapsed.count();
+//     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end -
+//     begin); cout << "insert time test took " << elapsed.count() << "
+//     nanoseconds" << endl; total += elapsed.count();
 //
 //     begin = std::chrono::high_resolution_clock::now();
 //     int _ = mq.min();
 //     end = std::chrono::high_resolution_clock::now();
-//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-//     cout << "min time test took " << elapsed.count() << " nanoseconds" << endl;
-//     total += elapsed.count();
+//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end -
+//     begin); cout << "min time test took " << elapsed.count() << "
+//     nanoseconds" << endl; total += elapsed.count();
 //
 //     begin = std::chrono::high_resolution_clock::now();
 //     _ = mq.extract_min();
 //     end = std::chrono::high_resolution_clock::now();
-//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-//     cout << "extract min time test took " << elapsed.count() << " nanoseconds" << endl;
-//     total += elapsed.count();
+//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end -
+//     begin); cout << "extract min time test took " << elapsed.count() << "
+//     nanoseconds" << endl; total += elapsed.count();
 //
 //     int last_index = size - 1;  // indexing starts at 0
 //     int new_val = mq.min() - 1; // ensure we're decreasing the key
 //     begin = std::chrono::high_resolution_clock::now();
 //     mq.decrease_key(last_index, new_val);
 //     end = std::chrono::high_resolution_clock::now();
-//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-//     cout << "decrease key time test took " << elapsed.count() << " nanoseconds" << endl;
-//     total += elapsed.count();
-//     cout << "Total time: " << total << endl;
+//     elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end -
+//     begin); cout << "decrease key time test took " << elapsed.count() << "
+//     nanoseconds" << endl; total += elapsed.count(); cout << "Total time: " <<
+//     total << endl;
 // }
 
-int main()
-{
+int main() {
 
-    // test_minqueue();
-    // test_insert();
-    // test_min();
-    // test_extract_min();
-    // test_decrease_key();
-    // test_heapify();
-    // test_build_min_heap();
-    // test_heapsort();
-    //
-    // test_application_waitlist();
-    //
-    // time_test();
+  // test_minqueue();
+  // test_insert();
+  // test_min();
+  // test_extract_min();
+  // test_decrease_key();
+  // test_heapify();
+  // test_build_min_heap();
+  // test_heapsort();
+  //
+  // test_application_waitlist();
+  //
+  // time_test();
 
-    cout << "Testing completed" << endl;
+  cout << "Testing completed" << endl;
 
-    return 0;
+  return 0;
 }
